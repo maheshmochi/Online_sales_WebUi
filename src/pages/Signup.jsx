a759362c458b6e282cf64059d84d683e3ebedb40
Mahesh <parmarmahesh.b1234@gmail.com>
2026-01-20 10:41:23 +0530

    Rename Signup.css to signup.css

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "./Signup.css";
+import "./signup.css";
 
 export default function Signup() {
   const [value, setValue] = useState("");
